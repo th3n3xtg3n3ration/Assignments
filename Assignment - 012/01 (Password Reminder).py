b078e3112b137a39e2b84bb0a5e4022240f42fe1
Uğur Çelebi <HasanUgur.CELEBI@vakifbank.com.tr>
2022-06-02 18:26:01 +0300

    first commit

---
+++ Assignment - 012/01 (Password Reminder).py
@@ -0,0 +1,19 @@
+"""
+Task : Let's say; you left a message in the past that prints a password you need. To see the password you wrote, you need to enter your name and the program should recognize you.
+
+Write a program that 
+
+    Takes the first name from the user and compares it to yours,
+    Then if the name the user entered is the same as yours, print out such as : "Hello, Joseph! The password is : W@12",
+    If the name the user entered is not the same as yours, print out such as : "Hello, Amina! See you later."
+"""
+
+name = "Joseph"
+password = "WQ12"
+
+first_name = input("Enter your first name: ")
+
+if first_name.title() == name :
+  print(f"Hello, Joseph! Then password is : {password} ")
+else :
+  print(f"Hello, {first_name}! See you later.")
