f75c20cf725fec1a69ae2d2dad1427e3675313aa
Uğur Çelebi <HasanUgur.CELEBI@vakifbank.com.tr>
2022-06-09 18:27:44 +0300

    Prime Numbers developed.

---
+++ Assignment - 012/06 (Prime Numbers).py
@@ -0,0 +1,25 @@
+"""
+Task : Print the prime numbers which are between 1 to entered limit number (n).
+
+1. You can use a nested for loop. 
+2. Collect all these numbers into a list
+
+The desired output for n=100 :
+
+[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
+61, 67, 71, 73, 79, 83, 89, 97]
+"""
+number = int(input("Please enter a range number: "))
+prime = []
+
+for i in range(0,number):
+    if i > 1:
+        for j in range(2,i):
+            if (i % j) == 0:
+                break
+        else:
+            prime += [i]
+    else:
+        print(number,"is not a prime range.")
+
+print(prime)
